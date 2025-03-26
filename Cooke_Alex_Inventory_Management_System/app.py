0be209078f7d8f12993f43ef54ed3d4adb6a46dc
Alex-Cooke192 <30250508@blackpool.ac.uk>
2025-03-26 11:52:17 +0000

    Update app.py
    Integrated Login System into top-down function. 
    
    Debugging: 
    - Couldn't access LoginSystem file from app.py as Login System was within another directory. 
    - Couldn't have 2 instances of Tk at the same time. Added 'result' as global variable, which determines whether or not the login was successful. This is then used in 'app.py' to enable login to main application window.

--- Cooke_Alex_Inventory_Management_System/app.py
+++ Cooke_Alex_Inventory_Management_System/app.py
@@ -5,6 +5,7 @@ import pathlib
 import json
 import time
 import os
+import sys
 
 from datetime import datetime
 
@@ -15,17 +16,24 @@ from file_manager import file_object
 from inventories_manager import inventories_manager
 import config
 
-# Create objects & relevant pathways between objects
-app_manager = admin()
-manager = inventories_manager()
-file_manager = file_object()
-interface = GUI(app_manager, manager, file_manager)
-manager.interface_init(interface)
-app_manager.screen_init(interface)
-try: 
-    config.inventories = file_manager.file_read_inventories(config.inventories)
-except UnboundLocalError:
-    pass
-print(config.inventories)
-interface.mainloop() 
+directory = os.getcwd()
+login_path = (f'{directory}/Login_System')
+print(login_path)
+sys.path.insert(0, login_path)
 
+import LoginSystem
+
+def grant_access():
+    # Create objects & relevant pathways between objects
+    app_manager = admin()
+    manager = inventories_manager()
+    file_manager = file_object()
+    interface = GUI(app_manager, manager, file_manager)
+    manager.interface_init(interface)
+    app_manager.screen_init(interface)
+    try: 
+        config.inventories = file_manager.file_read_inventories(config.inventories)
+    except UnboundLocalError:
+        pass
+    print(config.inventories)
+    interface.mainloop() 
